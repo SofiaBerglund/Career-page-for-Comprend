e9e4e922b52fa1f8c2ea6c85f70de9e6f3c202d8
kblanchfield <kateblanchfield@live.co.uk>
2018-10-14 19:44:28 +0200

    Updated career page css

--- src/components/colleague/colleague.js
+++ src/components/colleague/colleague.js
@@ -4,7 +4,6 @@ import "./colleague.css"
 export default class Colleague extends React.Component {
 
   reserveFaceUrls = [
-                    "/images/face-1.png",
                      "/images/face-2.png",
                      "/images/face-3.png",
                      "/images/face-4.png",
@@ -17,7 +16,7 @@ export default class Colleague extends React.Component {
         <div className="colleague-block" >
           <div className="image-block">
             <img className="colleague-image" src={this.props.source} alt={this.props.name} />
-            <img className="colleague-image-sw" src={this.reserveFaceUrls[Math.floor(Math.random()*6)]} />
+            <img className="colleague-image-sw" src={this.reserveFaceUrls[Math.floor(Math.random()*5)]} />
           </div>
           <p className="colleague-name">{this.props.name}</p>
           <p className="colleague-title">{this.props.title}</p>
--- src/pages/career-page/career-page.js
+++ src/pages/career-page/career-page.js
@@ -6,32 +6,49 @@ class CareerPage extends React.Component {
 
   render() {
     return (
-
-  <div className="careerPageContainer">
-    <div className="heroImage"><img src="/images/heroImage.svg" /></div>
-      <div className="hero-text">
-        <img src="/images/joinUs!.svg" />
-        <Link to="/jobs">
-        <p>Your journey starts here!</p>
-      </Link>
-    </div>
-
-
-    <div className="intro-container">
-        <div className="intro-text">
-            <h2>Helping our clients communicate</h2>
-            <p> Our belief is that communication takes place between people, increasing knowledge and encouraging action. For companies to be successful, they need employees, customers, suppliers and the public to know as much as possible about what the company does, how they do it and what they stand for.</p>
+      <div>
+        <div className="hero">
+          <div className="hero-text">
+            <h1>Careers at Comprend</h1>
+            <Link to="/jobs">
+              <button type="button">Current job openings</button>
+            </Link>
+          </div>
         </div>
-        <div className="intro-text">
-            <h2>This is Comprend</h2>
-            <p>We are a team of 80+ consultants in Stockholm and London. If you’re a curious and creative developer, designer, UX specialist, project manager or strategist and passionate about digital communication – get in touch to explore our world! Check out our jobs to see what new colleagues we are looking for right now.</p>
+        <div className="body-text">
+          <h2>At Comprend, we are always looking for great people and talents!</h2>
+          <p>We are currently 80 consultants with skills in digital strategy, project management, UX, visual design, development, content and research – all with a passion for digital communication.</p>
+          <p>We&apos;re located at offices in London and Stockholm. We work together with our partners Lundquist in Milan, as well as group partners in the H&H Group: Hallvarsson & Halvarsson, Intellecta Corporate, Jung Relations, Creo, Springtime, Hilanders, Tomorrow, Involve, Identx, Consilio och Bysted.</p>
+          <p>If you&apos;re curious, creative and passionate about digital communication – <Link to="/jobs">check what positions we&apos;re looking for right now</Link>.</p>
         </div>
-        <div className="intro-text">
-          <h2>We know how</h2>
-          <p>We understand the complex environment that large companies, listed companies and organisations operate in. We add more than 20 years of experience of digital corporate communication backed by research and insight, when working together with you to transform and manage your communication.</p>
+        <div className="image-block">
+        <h2>Follow us on Instagram! #ComprendLife</h2>
+          <div className="image-cropping-box">
+            <img src="/images/insta-1.jpg" alt="comprend-instagram-image" />
+          </div>
+          <div className="image-cropping-box">
+            <img src="/images/insta-2.jpg" alt="comprend-instagram-image" />
+          </div>
+          <div className="image-cropping-box">
+            <img src="/images/insta-3.jpg" alt="comprend-instagram-image" />
+          </div>
+          <div className="image-cropping-box">
+            <img src="/images/insta-4.jpg" alt="comprend-instagram-image" />
+          </div>
+          <div className="image-cropping-box">
+            <img src="/images/insta-5.jpg" alt="comprend-instagram-image" />
+          </div>
+          <div className="image-cropping-box">
+            <img src="/images/insta-6.jpg" alt="comprend-instagram-image" />
+          </div>
+          <div className="image-cropping-box">
+            <img src="/images/insta-7.jpg" alt="comprend-instagram-image" />
+          </div>
+          <div className="image-cropping-box">
+            <img src="/images/insta-8.jpg" alt="comprend-instagram-image" />
+          </div>
         </div>
       </div>
-  </div>
     )
   }
 
